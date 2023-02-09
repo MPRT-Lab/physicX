0349e530bb2dd0ac93b9ed9f7a925bfdf453a74c
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-02-10 03:28:01 +0330

    Constants Physics Fundamentals

--- c_c++/hi.c
+++ c_c++/hi.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+#include "constants.h"
 
 int main()
 {
-  printf("hello world");
+  printf("%d", c ** 2);
+  printf("%f", 1 / epsilon0);
+  printf("%f", h * F);
+  printf("%d", G);
   return 0;
 }
