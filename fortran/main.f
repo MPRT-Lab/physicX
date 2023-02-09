8871f2c64a684ed84a3636cf21265b94e180c68f
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-02-09 21:39:30 +0330

    Constants Physics Fundamentals

--- fortran/main.f
+++ fortran/main.f
@@ -1,4 +1,8 @@
 	PROGRAM HELLO
-	USE CONSTANTS
-	PRINT *, c
-	END PROGRAM HELLO
+        USE CONSTANTS
+        PRINT *,1/epsilon0
+        print *, h, rh
+        print *, G
+        print *, e * u0
+        print *, c ** 2
+        END PROGRAM HELLO
