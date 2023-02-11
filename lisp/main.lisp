552097afe58f1ec96350ba008ae49d0d72bff4b0
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-02-12 01:55:50 +0330

    Constants Physics Fundamentals

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -2,7 +2,7 @@
 
 (IN-PACKAGE constants)
 
-(FORMAT T "~% k = ~F" (/ 1 (* 4 pi epsilon0))) 
+(FORMAT T "~% k = ~F" (/ 1 (* 4 PI epsilon0))) 
 (PRINT K_J)
 (PRINT (/ 1 epsilon0)
 (WRITE h) (TERPRI) (WRITE rh)
