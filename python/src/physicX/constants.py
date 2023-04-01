9296eec58d847e7cdd49d3f9902411977a9b09ee
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-04-01 21:28:50 +0330

    Constants Physics Fundamentals

---
+++ python/src/physicX/constants.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+c = 299792458
+G = 6.674e-11
+N_A = 6.02214076E23
+e = 1.602176634E-19
+m_e = 9.1093837015E-31
+h = 6.62607015E-34
+rh = h/(2 * np.pi)
+u0 = 4 * np.pi * 1E-7
+epsilon0 = 1/((c**2) * u0)
+k = 1/(4 * np.pi * epsilon0)
+f = e * N_A
+K_J = (2 * e)/h
+R_K = h/e**2
+alpha = e**2/(2 * epsilon0 * (h * c))
+
