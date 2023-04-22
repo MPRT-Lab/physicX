e4a1a97cea1af79682eb0e8555faaf6683f7fc0c
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-04-23 00:03:29 +0330

    Constants Physics Fundamentals

---
+++ java/Constants.java
@@ -0,0 +1,13 @@
+package physicX.Constants;
+
+import java.util.*;
+
+public class Constants
+{
+	public static double G = 6.674e-11;
+	public static double c = 299792458;
+	public static double h = 6.62607015e-34;
+	public static double N_A = 6.02214076e23;
+	public static double e = 1.602176634e-19;
+	
+}
