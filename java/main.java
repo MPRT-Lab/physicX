18352c0b7c4cd46cc928817ce9338442e3cb390d
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-04-23 00:06:05 +0330

    Constants Physics Fundamentals

---
+++ java/main.java
@@ -0,0 +1,10 @@
+package main;
+import physicX.Constants.*;
+
+public class main{
+	public static void main(String args[])
+	{
+		Constants con = new Constants();
+		System.out.println(con.G);
+	}
+}
