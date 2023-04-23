c37ecd399ff863682f374c52603214cd13508802
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-04-23 04:06:50 +0330

    Constants Physics Fundamentals

--- java/main.java
+++ java/main.java
@@ -1,10 +1,11 @@
-package main;
 import physicX.Constants.*;
 
 public class main{
 	public static void main(String args[])
 	{
 		Constants con = new Constants();
-		System.out.println(con.G);
+		System.out.println(con.G * con.c);
+		System.out.println(con.h/con.e);
+		System.out.println(con.kaka);
 	}
 }
