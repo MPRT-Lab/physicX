95745ca75e60617be739695c1c2afd9fb5ef9739
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-04-23 04:02:49 +0330

    Constants Physics Fundamentals

--- java/Constants.java
+++ java/Constants.java
@@ -8,10 +8,8 @@ public class Constants
 	public static double G = 6.674e-11;
 	public static double c = 299792458;
 	public static double h = 6.62607015e-34;
-	public static double N_A = 6.02214076e23;
 	public static double e = 1.602176634e-19;
 	public static double N_A = 6.02214076e23;
-	public static double e = 1.602176634e-19;
 	public static double R = 8.31446261815324;
 	public static double m_e = 9.1093837015e-31;
 	public static double rh = h / (2 * Math.PI);
