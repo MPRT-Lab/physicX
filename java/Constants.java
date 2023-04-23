1a32ce34ec55b413709846eeba72b20f7d2eaae4
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-04-23 03:51:07 +0330

    Constants Physics Fundamentals

--- java/Constants.java
+++ java/Constants.java
@@ -4,10 +4,26 @@ import java.util.*;
 
 public class Constants
 {
+	public static double h = 6.62607015e-34;
 	public static double G = 6.674e-11;
 	public static double c = 299792458;
 	public static double h = 6.62607015e-34;
 	public static double N_A = 6.02214076e23;
 	public static double e = 1.602176634e-19;
+	public static double N_A = 6.02214076e23;
+	public static double e = 1.602176634e-19;
+	public static double R = 8.31446261815324;
+	public static double m_e = 9.1093837015e-31;
+	public static double rh = h / (2 * Math.PI);
+	public static double F = e * N_A;
+	public static double u0 = (4 * Math.PI * Math.pow(10,-7));
+	public static double epsilon0 = 1 / (Math.pow(c,2) * u0);
+	public static double k = 1/(4 * Math.PI * epsilon0);
+	public static double k_B = R/N_A;
+	public static double K_J = (2 * e)/h;
+	public static double R_K = h/Math.pow(e,2);
+	public static double kaka = (8 * Math.PI * G)/Math.pow(c,4);
+	public static double alpha = Math.pow(e,2)/ (2 * epsilon0 * (h * c));
+	public static double R_inf = (m_e * Math.pow(e,4)) / (8 * Math.pow(epsilon0,2) * Math.pow(h,3) * c);
 	
 }
