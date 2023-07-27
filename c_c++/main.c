c1131e20712befc35e7eae222bf7147a60682603
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-07-27 05:19:31 -0400

    Functions of the laws of physics (test)

--- c_c++/main.c
+++ c_c++/main.c
@@ -2,10 +2,6 @@
 
 int main()
 {
-  printf("%f\n", pow(c,2));
-  printf("%.15f\n", 1 / epsilon0);
-  printf("%.34f\n", h * F);
-  printf("%.15f", G);
-  printf("%f", hubble(30));
+  printf("%f", hubble(1.3642));
   return 0;
 }
