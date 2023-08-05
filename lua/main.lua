69c6c1ffab6909026d28eaa7a1c11de3036fb468
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-08-06 01:42:21 +0330

    Functions of the laws of physics

--- lua/main.lua
+++ lua/main.lua
@@ -1,7 +1,10 @@
-con = require("constants")
-
+require("physicX")
+con = constants
+law = law
 print (1/con.epsilon0)
 print (con.h,con.rh)
 print (con.G)
 print (con.e * con.u0)
 print (con.c ^ 2)
+
+print(law.hubble(30))
