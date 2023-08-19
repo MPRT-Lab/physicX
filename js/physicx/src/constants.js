26557d7f6525b85eb133908f4f2f53e1d3d3cc93
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-08-19 04:57:20 +0330

    Constants Physics Fundamentals

--- js/physicx/src/constants.js
+++ js/physicx/src/constants.js
@@ -3,7 +3,7 @@ const Constants = new function () {
 	  this.c = 299792458;
 	  this.u0 = (4 * Math.PI * 10 ** (-7));
 	  this.epsilon0 = 1 / (this.c**2 * this.u0);
-	  this.k_e = 1/(4 * Math.PI * this.epsilon0);
+	  this.k = 1/(4 * Math.PI * this.epsilon0);
 	  this.h = 6.62607015e-34;
 	  this.rh = this.h / (2 * Math.PI);
 	  this.N_A = 6.02214076e23;
@@ -17,6 +17,5 @@ const Constants = new function () {
 	  this.kaka = (8 * Math.PI * this.G)/this.c**4;
 	  this.alpha = this.e**2/ (2 * this.epsilon0 * (this.h * this.c));
 	  this.R_inf = (this.m_e * (this.e ** 4))/(8 * (this.epsilon0 ** 2) * (this.h ** 3) * this.c);
-	  this.k = this.R / this.N_A;
 }
 export default Constants;
