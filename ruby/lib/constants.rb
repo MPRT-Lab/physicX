059fedca0b30f35a6e117d823b81d853db53e247
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-08-20 05:27:39 +0330

    Constants Physics Fundamentals

--- ruby/lib/constants.rb
+++ ruby/lib/constants.rb
@@ -2,10 +2,12 @@ module Constants
 	Speedoflight	= 299792458
 	G 		= 6.674e-11
 	N_A		= 6.02214076e23
+	K_CD		= 683
 	E		= 1.602176634e-19
 	M_E		= 9.1093837015E-31
 	H		= 6.62607015E-34
 	R		= 8.31446261815324
+	B		= 2.897771955
 	K_B		= R/N_A
 	Rh		= H/(2 * Math::PI)
 	U0		= (4 * Math::PI * 1e-7)
@@ -18,6 +20,6 @@ module Constants
 	Alpha		= E**2/(2 * Epsilon0 * (H * Speedoflight))
 	R_inf		= (M_E * (E ** 4)) / (8 * (Epsilon0 ** 2) * (H ** 3) * Speedoflight)	
 	H_0		= 66.6
+	Sigma		= (2 * Math::PI**5 * K_B**4)/(15 * H**3 * Speedoflight**2)
 end
-
 
