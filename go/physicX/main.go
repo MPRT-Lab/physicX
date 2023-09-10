e97991fd6fdbdc29f65415cc9587ef32f361f65b
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-09-10 15:51:49 +0330

    Functions of the law's and constants of physics (test)

--- go/physicX/main.go
+++ go/physicX/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"physicX/law"
 	"physicX/constants"
+	"physicX/mathlib"
 )
 
 func main() {
 	fmt.Println(constants.Sigma)
 	fmt.Println(law.Hubble(30))
+	fmt.Println(mathlib.Summation(1,10,1))
+	fmt.Println(mathlib.Factorial(5))
 }
