8e216dffeba2904bc66233c1c4f0399e140e1203
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-09-16 02:26:53 +0330

    test library physicX

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,3 +1,4 @@
+import Physicx.Mathlib
 import Physicx.Constants
 main = do
 	print (1/epsilon0)
@@ -6,3 +7,7 @@ main = do
 	print (g)
 	print (e * u0)
 	print (c ** 2)
+	print (fibonacci(5))
+	print(gcd 10 45)
+	print(ack 2 3)
+	print(summation 1 10)
