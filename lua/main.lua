c3c7a177ed1aa337b68bd7f205c1817b6a6b204e
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-09-16 03:24:23 +0330

    Test  physicX library

--- lua/main.lua
+++ lua/main.lua
@@ -1,10 +1,12 @@
 require("physicX")
 con = constants
 law = law
+maths = mathlib
 print (1/con.epsilon0)
 print (con.h,con.rh)
 print (con.G)
 print (con.e * con.u0)
 print (con.c ^ 2)
-
+print (maths.fibonacci(8))
+print (maths.summation(1,10))
 print(law.hubble(30))
