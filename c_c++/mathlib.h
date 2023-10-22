27c6569b7a85e9548f86a3dd9b3c1a4122082c4b
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-10-22 03:21:52 -0400

    Mathematics library for C/C++

--- c_c++/mathlib.h
+++ c_c++/mathlib.h
@@ -3,6 +3,8 @@
 #ifndef MATHEMATICS_H
 #define MATHEMATICS_H
 int summation(int i, int n, int x);
-
+int factorial(int n);
+int ack(int m, int n);
+unsigned long int catalan(long int n);
+#define MAX 500
 #endif
-
