2da3234da44bc16ac44803aa52da21474b1cfd22
cunknown <48595746+anonymous14725@users.noreply.github.com>
2023-10-22 03:21:22 -0400

    Mathematics library for C/C++

--- c_c++/mathlib.c
+++ c_c++/mathlib.c
@@ -23,3 +23,33 @@ int summation(int i, int n, int x)
 	return 0;
 }
 
+int factorial(int n)
+{
+  if (n == 1) return 1;
+  else return n * factorial(n - 1);
+}
+
+int ack(int m, int n)
+{
+    if (m == 0){
+        return n + 1;
+    }
+    else if((m > 0) && (n == 0)){
+        return ack(m - 1, 1);
+    }
+    else if((m > 0) && (n > 0)){
+        return ack(m - 1, ack(m, n - 1));
+    }
+    return 0;
+}
+
+unsigned long int catalan(long int n) 
+{
+   if (n <= 1) 
+	   return 1;
+   unsigned long int res = 0;
+   for (int i = 0;i < n; i++){
+      	res += catalan(i) * catalan(n - i - 1);
+   }
+   return res;
+}
