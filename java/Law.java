9b5a4dc871ec5a03c4a973b9095e0ac353019eb4
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-02-29 04:55:50 +0330

    Functions of the laws of physics

--- java/Law.java
+++ java/Law.java
@@ -11,4 +11,10 @@ public class Law
 		Constants cons = new Constants();
 		return cons.H_0 * D;
 	}
+	
+	public double planck(double nu, int T)
+	{
+		Constants cons = new Constants();
+		return (8 * Math.PI * Math.pow(nu,2) * cons.h * nu)/(Math.pow(cons.c,3) * Math.exp((cons.h * nu)/(cons.k_B * T)) - 1);
+	}
 }
