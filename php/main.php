092a416975994638b74dd4f7637029b02c27d504
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-02-29 05:14:56 +0330

    Functions of the laws of physics (test)

--- php/main.php
+++ php/main.php
@@ -1,6 +1,7 @@
 <?php require "physicX.php"; ?>
 <?php
-	echo hubble(30);
-	echo c ** 2;
-	echo sigma . " " . alpha;
+	echo hubble(30) . "\n";
+	echo planck(1e-15,7500) . "\n";
+	echo c ** 2 . "\n";
+	echo sigma . "\n" . alpha;
 ?>
