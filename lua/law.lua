5dfa8ecdd9adad4afa5022da26b04a33710ca9c2
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-02-29 05:22:00 +0330

    Functions of the laws of physics

--- lua/law.lua
+++ lua/law.lua
@@ -4,4 +4,8 @@ const = require("constants")
 function law.hubble(D)
 	return const.H_0 * D
 end
+
+function law.planck(nu, T)
+	return (8 * math.pi * nu^2 * const.h * nu)/(const.c^3 * math.exp((const.h * nu)/(const.k_B * T)) - 1)
+end
 return law
