f7d919b68d59a3d16611d3538b4109ebae4d27a4
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-02-29 05:39:44 +0330

    Functions of the laws of physics

--- python/src/physicX/law.py
+++ python/src/physicX/law.py
@@ -5,3 +5,7 @@ from .constants import *
 ### Hubble law's 
 def hubble(D):
 	return H_0 * D
+
+### Planck law's
+def planck(nu, T):
+	return (8 * np.pi * nu**2 * h * nu)/(c**3 * np.exp((h * nu)/(k_B * T)) - 1)
