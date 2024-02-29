9bb883212f43cc5f9d998411229a24870615a144
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-02-29 05:41:07 +0330

    Functions of the laws of physics (test)

--- python/test/main.py
+++ python/test/main.py
@@ -1,7 +1,8 @@
 import physicX.constants as con
-
+import physicX.law as law
 print(1/con.epsilon0)
 print(con.h,con.rh)
 print(con.G)
 print(con.e * con.u0)
 print(con.c ** 2)
+print(law.planck(1e-15,7500))
