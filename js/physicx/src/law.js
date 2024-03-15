186fdf548b74ca24c3001b41e88b2c52f1f8578d
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-03-15 05:00:51 +0330

    Functions of the laws of physics

--- js/physicx/src/law.js
+++ js/physicx/src/law.js
@@ -4,6 +4,9 @@ const Law = new function () {
 	this.Hubble = function (D) {
 		return D * Constants.H_0;
 	}
+	this.Planck = function (nu, T) {
+		return (8 * Math.PI * nu ** 2 * Constants.h * nu) / (Constants.c ** 3 * Math.exp((Constants.h * nu) / (Constants.k_B * T)) - 1);
+	}
 }
 
 export default Law;
