8dc014cd1c9eb4d6b2a2afd95ac7df437c5944fc
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-07-24 21:02:14 +0330

    Functions of the laws of physics

--- lisp/law.lisp
+++ lisp/law.lisp
@@ -5,3 +5,8 @@
 	(* constants::H_0 D)
 )
 
+(DEFUN planck (nu Temp)
+  (/ (* 8 PI (expt nu 2) constants::h nu)
+     (- (expt constants::c 3) (EXP (/ (* constants::h nu) (* constants::k_B Temp)))))
+)
+
