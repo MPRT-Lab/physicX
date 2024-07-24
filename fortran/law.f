67cb306d2de410d821e766baccfc9ee1f67b4c8c
cunknown <48595746+anonymous14725@users.noreply.github.com>
2024-07-24 04:09:57 +0330

    Functions of the laws of physics

--- fortran/law.f
+++ fortran/law.f
@@ -6,5 +6,13 @@
 			REAL :: D
 			HUBBLE = D * H_0
 		END FUNCTION HUBBLE
-
+  
+    		FUNCTION PLANCK(NU, T)
+			REAL :: NU, T
+			REAL(KIND=8) :: RAD, EXPS
+			EXPS = EXP((h * NU) / (k_B * T))
+			RAD = (8 * PI * NU**2 * h * NU) / (c**3 * EXPS - 1)
+			PLANCK = RAD
+		END FUNCTION PLANCK
+  
 	END MODULE LAW
